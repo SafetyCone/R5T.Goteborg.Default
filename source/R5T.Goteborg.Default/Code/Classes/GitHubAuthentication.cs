3b0c1b6c523bd06013ac829fe4d331d7f9e39acb
MinexAutomation <minex.automation@gmail.com>
2019-11-18 22:48:53 -0800

    Added GitHub authentication.

---
+++ source/R5T.Goteborg.Default/Code/Classes/GitHubAuthentication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Goteborg.Default
+{
+    public class GitHubAuthentication
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
